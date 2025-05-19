694c009b5e50568819de86547b15f0457ca6960b
Patrick Schiller <patrick.schiller@dacs-bund.de>
2025-05-19 18:02:07 +0200

    clean data_view.py

--- data_view.py
+++ data_view.py
@@ -1,13 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-
 st.title('ASAP')
 st.subheader('Android Sensor Analysis Platform')
-
-@st.cache_data
-def get_accelerometer_df(input_df) -> pd.DataFrame:
-  return input_df.set_index('sensor').filter(like='Accelerometer', axis=0)
 
 sensor_data = st.sidebar.file_uploader(
   "Upload a sensor.csv file",
@@ -21,8 +16,10 @@ location_data = st.sidebar.file_uploader(
 
 if sensor_data is not None:
   # data: sensor name, timestamp
-  sdf = pd.read_csv(sensor_data, names=['sensor','timestamp','value0','value1','value2','value3','value4','value5','value6','value7','value8','value9','value10','value11','value12','value13','value14','value15'])
-  sdf['timestamp'] = pd.to_datetime(sdf['timestamp'],unit='ms')
+  sdf = pd.read_csv(sensor_data,
+                    names=['sensor','timestamp','value0','value1','value2','value3','value4','value5','value6','value7','value8','value9','value10','value11','value12','value13','value14','value15'],
+                    index_col='timestamp')
+  sdf.index = pd.to_datetime(sdf.index,unit='ms')
   st.dataframe(sdf)
 
   sensors_df = sdf.sensor.unique()
@@ -32,5 +29,5 @@ if location_data is not None:
   ldf = pd.read_csv(location_data, names=['timestamp', 'latitude', 'longitude'])
   ldf['timestamp'] = pd.to_datetime(ldf['timestamp'],unit='ms')
   st.dataframe(ldf)
-  # fourth decimal for coordinates correlates to 11.1m, thus the size
+  # fourth decimal for coordinates is tracked and correlates to 11.1m, thus the size
   st.map(ldf, size=11)
