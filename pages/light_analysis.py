417ce7af243915a47eb9dae15575bddccb8aae37
Patrick Schiller <patrick.schiller@dacs-bund.de>
2025-05-19 20:47:43 +0200

    rename light value

--- pages/light_analysis.py
+++ pages/light_analysis.py
@@ -13,7 +13,7 @@ st.subheader('Analysis of the Light Sensor')
 if sensor_data is not None:
   # data: sensor name, timestamp
   sdf = pd.read_csv(sensor_data,
-                    names=['sensor','timestamp','value0','value1','value2','value3','value4','value5','value6','value7','value8','value9','value10','value11','value12','value13','value14','value15'],
+                    names=['sensor','timestamp','light_level','value1','value2','value3','value4','value5','value6','value7','value8','value9','value10','value11','value12','value13','value14','value15'],
                     index_col='timestamp')
   # timezones tracked only as utc as of now
   sdf.index = pd.to_datetime(sdf.index, unit='ms', utc=True)
@@ -21,4 +21,4 @@ if sensor_data is not None:
   sdf = sdf[sdf['sensor'].str.endswith("Light Sensor")]
   st.write(f'{sdf.shape[0]} rows found')
   st.dataframe(sdf)
-  st.line_chart(sdf, y='value0')
+  st.line_chart(sdf, y='light_level')
